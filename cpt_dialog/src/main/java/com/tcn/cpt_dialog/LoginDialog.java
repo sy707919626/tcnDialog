99fe074141d11fe3b4d753fc6650ecb4c7e7be57
sy707919626 <707919626@qq.com>
2021-02-23 16:11:19 +0800

    修改

--- cpt_dialog/src/main/java/com/tcn/cpt_dialog/LoginDialog.java
+++ cpt_dialog/src/main/java/com/tcn/cpt_dialog/LoginDialog.java
@@ -10,6 +10,8 @@ import android.widget.ImageView;
 public class LoginDialog extends Dialog {
     private ImageView closeImage;
     private Button button;
+    private int resouceId = 0;
+    private Context mContext;
 //
 //    private callBackButton mCallback;
 //    public void setCallback(callBackButton callback) {
@@ -22,12 +24,24 @@ public class LoginDialog extends Dialog {
 
     public LoginDialog(Context context) {
         super(context);
+        mContext = context;
+    }
+
+    public LoginDialog(Context context, int ResouceId) {
+        super(context);
+        mContext = context;
+        resouceId = ResouceId;
     }
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.login_dialog);
+        if (resouceId == 0) {
+            setContentView(R.layout.login_dialog);
+        } else {
+            setContentView(resouceId);
+        }
+
         closeImage = findViewById(R.id.close);
         button = findViewById(R.id.select_btn);
 //        button.setBackground(getContext().getDrawable(R.drawable.btn_blue_border_selector));
